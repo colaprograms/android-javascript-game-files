9f4d9ac401f47d895b31d244a55f5953f0179f22
Hannah Cairns <hannah.abigail.cairns@gmail.com>
2019-08-03 15:54:38 -0400

    error

--- handle-error-event.js
+++ handle-error-event.js
@@ -5,14 +5,14 @@ window.addEventListener(
     function(e) {
       if(window.android && window.android.showexception) {
         console.log(e);
+        for(var property in e) {
+            console.log(`${property}: ${e[property]}`);
+        }
+        //console.log(e);
         e.preventDefault();
         if(e.error) {
           android.showexception(`${e.message}\n File: ${e.filename}\n Line: ${e.lineno}\n Column: ${e.colno}\nStack:\n${e.error.stack}`);
         }
-        else {
-          android.showexception(`${e.message}\n File: ${e.filename}\n Line: ${e.lineno}\n Column: ${e.colno}`);
-        }
-      }
     },
     true
 );
