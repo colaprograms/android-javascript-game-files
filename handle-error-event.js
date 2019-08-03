3e4e7c54a14d7867cb1707b17020895eeb33da34
Hannah Cairns <hannah.abigail.cairns@gmail.com>
2019-08-03 16:05:14 -0400

    rar

--- handle-error-event.js
+++ handle-error-event.js
@@ -5,10 +5,6 @@ window.addEventListener(
     function(e) {
       if(window.android && window.android.showexception) {
         console.log(e);
-        for(var property in e) {
-            console.log(`${property}: ${e[property]}`);
-        }
-        //console.log(e);
         e.preventDefault();
         if(e.error) {
           android.showexception(`${e.message}\n File: ${e.filename}\n Line: ${e.lineno}\n Column: ${e.colno}\nStack:\n${e.error.stack}`);
